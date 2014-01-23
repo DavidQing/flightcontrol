24a8a0e36842636b9fc45db901df6908c7fea876
DavidQing <davidqing1991@gmail.com>
2014-01-22 19:17:22 -0500

    Create ThinkBowl_ITG3205_example.py

---
+++ ThinkBowl_ITG3205_example.py
@@ -0,0 +1,17 @@
+from ThinkBowl_ITG3205 import ITG3205
+from time import *
+
+gyro = ITG3205(0x68)
+
+while True:
+	(itgready, dataready) = gyro.getInterruptStatus()	
+	if dataready:
+		temp = gyro.getDieTemperature()
+		(x, y, z) = gyro.getDegPerSecAxes() 
+		print("Temp: "+str(temp))
+		print("X:    "+str(x))
+		print("Y:    "+str(y))
+		print("Z:    "+str(z))
+		print("")
+
+	sleep(1)
