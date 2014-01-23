ffb958d4c4518c962721f1b1a680422138117c8f
DavidQing <davidqing1991@gmail.com>
2014-01-22 19:25:19 -0500

    Update ThinkBowl_ITG3205_example.py

--- ThinkBowl_ITG3205_example.py
+++ ThinkBowl_ITG3205_example.py
@@ -6,9 +6,9 @@ gyro = ITG3205(0x68)
 while True:
 	(itgready, dataready) = gyro.getInterruptStatus()	
 	if dataready:
-		temp = gyro.getDieTemperature()
+		#temp = gyro.getDieTemperature()
 		(x, y, z) = gyro.getDegPerSecAxes() 
-		print("Temp: "+str(temp))
+		#print("Temp: "+str(temp))
 		print("X:    "+str(x))
 		print("Y:    "+str(y))
 		print("Z:    "+str(z))
